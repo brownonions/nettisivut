f526e1e6587073d485969e0858a7ef4361ed2d43
brownonions <54271324+brownonions@users.noreply.github.com>
2023-05-23 22:52:30 +0300

    Initial commit

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,3 +1,6 @@
+document.oncontextmenu = function() {
+  return false;
+}
 /*!
   * Bootstrap v5.0.2 (https://getbootstrap.com/)
   * Copyright 2011-2021 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
